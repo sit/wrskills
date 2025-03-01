b4a06211d1ae12a70fdd0ec710eda9bb7b72a6b6
Emil Sit <sit@emilsit.net>
2025-02-28 21:55:00 -0500

    rework using tailwind, daisy and token-template bits

---
+++ src/consts.ts
@@ -0,0 +1,25 @@
+export const SITE_TITLE = "Wild Rift Skills";
+export const SITE_DESCRIPTION =
+  `Learn Wild Rift Skills through Interactive Problem Solving`.trim();
+
+export const KNOWN_TECH =
+  `Astro,Tailwindcss,Svelte,Nextjs,Javascript,Typescript,Python,SCSS,CSS,HTML,HTMX,Astro,Tailwindcss,Svelte,Nextjs,Javascript,Typescript,Python,SCSS,CSS,HTML,HTMX`.split(
+    ",",
+  );
+export const ABOUT_ME =
+  `WR Skills is awesome`.trim();
+export const GITHUB_USERNAME = "sit";
+export const QUOTE = "Always be learning";
+export const NAV_LINKS: Array<{ title: string; href?: string }> = [
+  {
+    title: "Skills",
+  },
+  {
+    title: "Fire",
+    href: "//wildriftfire.com/",
+  },
+  {
+    title: "Guides",
+    href: "//wildriftguides.com/",
+  },
+];
